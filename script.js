64fee2cbb1dced31eeea1555bd743658d7298355
Alliened <storytimesaddness@gmail.com>
2026-04-16 08:00:11 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Year in footer
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
